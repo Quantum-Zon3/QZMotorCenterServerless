11653de27a8e9ce1a8a1d61c1625695d5b037f0f
CarlosMPatino7822 <carlosmariopatinoramirez@gmail.com>
2026-05-25 01:43:35 -0500

    AGREGADO funcionalidad con serverless

--- servicios/ServicioCorreo.js
+++ servicios/ServicioCorreo.js
@@ -1,5 +1,5 @@
-import { enviarCorreo } from "../utilidades/ConfiguracionCorreo.js";
 import { plantillaBienvenido } from "../plantillas/PlantillaBienvenido.js";
+import { enviarCorreo } from "../utilidades/ConfiguracionCorreo.js";
 
 export const procesarEnvioCorreo = async (email, tipo) => {
   let contenido;
@@ -12,17 +12,24 @@ export const procesarEnvioCorreo = async (email, tipo) => {
     case "compra":
       contenido = {
         subject: "Compra realizada",
-        text: "Tu compra fue exitosa 🚗🏍️",
+        text: "Tu compra fue exitosa.",
+      };
+      break;
+
+    case "inicio_sesion":
+      contenido = {
+        subject: "Inicio de sesion en QZ Motor Center",
+        text: "Se inicio sesion correctamente en tu cuenta de QZ Motor Center.",
       };
       break;
 
     default:
-      throw new Error("Tipo de correo no válido");
+      throw new Error("Tipo de correo no valido");
   }
 
   const mensaje = {
     to: email,
-    from: "tutienda@email.com",
+    from: process.env.SENDGRID_FROM_EMAIL || "tutienda@email.com",
     subject: contenido.subject,
     text: contenido.text,
   };
